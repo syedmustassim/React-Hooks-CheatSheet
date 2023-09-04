245fcfbde85d3d02950a8c6ba81e5cdc66dac510
Syed Mustassim Mustaq <111407440+syedmustassim@users.noreply.github.com>
2023-09-04 18:14:42 +0000

    feat: initial routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import Home from "./Home";
+import AppRouter from "./Routes/AppRouter";
 import "./styles.css";
-import UseMemoOne from "./useMemo/UseMemoOne";
-import RandomNumberGenerator from "./useRef/RandomNumberCounter";
-import RenderCount from "./useRef/RenderCounter";
-import Timer from "./useRef/Timer";
-import UseRefHook from "./useRef/UseRefHook";
 
 export default function App() {
   return (
     <div className="App">
-      <Home/>
-      {/* <UseRefHook />
-      <RandomNumberGenerator />
-      <RenderCount/> */}
-      {/* <Timer/> */}
-      <UseMemoOne/>
+      <AppRouter/>
     </div>
   );
 }
