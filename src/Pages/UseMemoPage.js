b62880a46ad1eb1f3e2449420ead522cdded86f4
Syed Mustassim Mustaq <111407440+syedmustassim@users.noreply.github.com>
2023-09-05 17:28:48 +0000

    feat: usememo docs

--- src/Pages/UseMemoPage.js
+++ src/Pages/UseMemoPage.js
@@ -2,6 +2,11 @@ const UseMemoPage = () => {
     return(
         <div>
             <h1> UseMemo Hook </h1>
+            <p> useMemo is a React hook that is used to memoize a heavy function. </p>
+            <p> Any function that demands heavy performance and leads to slower rendering times, is a heavy function. </p>
+            <p> As per the docs however, 'useMemo is a React Hook that lets you cache the result of a calculation between re-renders.'</p>
+            <p> This is a complex definition. </p>
+            <a href="https://react.dev/reference/react/useMemo">Link to useMemo docs</a>
         </div>
     )
 }
