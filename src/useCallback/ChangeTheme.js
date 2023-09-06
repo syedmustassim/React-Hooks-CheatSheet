89fde54f4a83feddd300fe18f643464ffd4e61db
Syed Mustassim Mustaq <111407440+syedmustassim@users.noreply.github.com>
2023-09-06 17:59:39 +0000

    feat: added example

--- src/useCallback/ChangeTheme.js
+++ src/useCallback/ChangeTheme.js
@@ -1,17 +1,25 @@
 import {useState} from "react"
-
+import List from "./List";
 const ChangeTheme = () => {
-    const [number, setNumber] = useState(1);
+    const [number, setNumber] = useState(0);
     const [dark, setDark] = useState(false);
 
     const theme = {
         backgroundColor: dark ? "#333" : "#FFF",
         color: dark ? "#FFF" : "#333"
     }
+
+    const getItems = () => {
+        return [number, number + 1, number + 2]
+    }
     return(
         <div style={theme}>
             <h1> Change theme example for useCallback </h1>
+            <p> To accurately check what happens here, we will have to check our console statements </p>
+            
+            <input type="text" onChange={e => setNumber(parseInt(e.target.value))} placeholder="Enter number"/>
             <button onClick={() => setDark(prev => !prev)}>Toggle theme</button>
+            <List getItems={getItems}/>
         </div>
     )
 }
---
+++ src/useCallback/List.js
@@ -0,0 +1,20 @@
+import {useState, useEffect} from "react"
+
+const List = ({getItems}) => {
+    const [items, setItems] = useState([])
+    const [render, setRender] = useState(0)
+
+    useEffect(() => {
+        setItems(getItems())
+        console.log('Updating items')
+        setRender(prev => prev + 1)
+    },[getItems])
+    return(
+        <div>
+            {items.map((item) => <div key={item}> <h2> {item} </h2></div>)}
+            <h2> The component has rendered {render} times </h2>
+        </div>
+    )
+}
+
+export default List;
