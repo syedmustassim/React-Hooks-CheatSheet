4a47a54848355a1583cc153ecc630763c76571e3
Syed Mustassim Mustaq <111407440+syedmustassim@users.noreply.github.com>
2023-09-18 16:39:41 +0000

    feat: render details

--- src/useRef/RenderCounter.js
+++ src/useRef/RenderCounter.js
@@ -17,6 +17,11 @@ const RenderCount = () => {
         <div>
         <Link to="/useref">Back to useRef hook</Link>
          <h1> Render Count </h1>
+         <p> This component displays the render count, every time we enter any details in the input field.</p>
+         <p>To keep track of the render count, useRef is used. </p>
+         <p> The main reason for this being, that a change in the useRef current value does NOT re-render the entire component.</p>
+         <p> Had we used the useState hook here to count the renders, for every change in the input field which creates a re-render, 
+            and then the state changes, that will again cause a re-render. This will actually lead to infinite renders. </p>
          <input type="text" onChange={e => setGame(e.target.value)}/>
          <h1>Selected game is - {game}</h1>
         <h2> The component has rendered {renderCount.current} number of times </h2>
